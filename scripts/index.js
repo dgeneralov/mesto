ac03778219e0096814fc35828a8b62703274aadf
dgeneralov <runnerforrest@gmail.com>
2021-03-19 20:58:43 +0300

    valid

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,28 @@
-let
-    popup = document.querySelector(".popup"),
+let popup = document.querySelector(".popup"),
     title = document.querySelector(".profile__title"),
     buttonEdit = document.querySelector(".profile__buttons-edit"),
     popupClouse = document.querySelector(".popup__clouse"),
     buttonSave = popup.querySelector(".popup__button"),
     subtitle = document.querySelector(".profile__subtitle"),
     like = document.querySelectorAll(".elements__icon");
-;
-
 for (i = 0; i < like.length; i++) {
     like[i].onclick = function () {
-
         if (this.getAttribute("src") == "./images/elenents-icon-active.svg") {
             this.setAttribute("src", "./images/elements-like.svg");
-
-        } else
-            this.setAttribute("src", "./images/elenents-icon-active.svg");
+        } else this.setAttribute("src", "./images/elenents-icon-active.svg");
         this.style.opacity = "1";
-    }
+    };
     like[i].onmouseover = function () {
         if (this.getAttribute("src") == "./images/elements-like.svg") {
             this.style.opacity = "0.6";
-        } else
-            this.style.opacity = "1";
-    }
+        } else this.style.opacity = "1";
+    };
     like[i].onmouseout = function () {
         if (this.getAttribute("src") == "./images/elements-like.svg") {
             this.style.opacity = "1";
-        } else
-            this.style.opacity = "1";
-    }
-
+        } else this.style.opacity = "1";
+    };
 }
-
-
 
 buttonEdit.addEventListener("click", function () {
     popup.classList.add("popup_opened");
@@ -43,11 +32,9 @@ popupClouse.addEventListener("click", function () {
     popup.classList.remove("popup_opened");
 });
 
-
 let formElement = document.querySelector(".popup__container");
 let nameInput = formElement.querySelector("#name");
 let jobInput = formElement.querySelector("#jobe");
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -56,10 +43,9 @@ function formSubmitHandler(evt) {
     document.querySelector(".profile__subtitle").textContent = jobInput.value;
 
     popup.classList.remove("popup_opened");
-
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
 
 
 
