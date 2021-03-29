ab377752aedf25be35a0875cf39db963b13b6394
dgeneralov <runnerforrest@gmail.com>
2021-03-29 14:31:05 +0300

    sec rev

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,31 @@
 let popup = document.querySelector(".popup"),
-    main = document.querySelector(".main"),
-    header = document.querySelector(".header"),
     title = document.querySelector(".profile__title"),
     buttonEdit = document.querySelector(".profile__buttons-edit"),
-    popupClouse = document.querySelector(".popup__clouse"),
+    popupClose = document.querySelector(".popup__close"),
     subtitle = document.querySelector(".profile__subtitle"),
-    like = document.querySelectorAll(".elements__icon"),
     formElement = document.querySelector(".popup__container"),
     nameInput = formElement.querySelector("#name"),
     jobInput = formElement.querySelector("#jobe");
 
-nameInput.value = "Жак-Ив Кусто";
-jobInput.value = "Исследователь океана";
-
 function openPopup() {
     popup.classList.add("popup_opened");
-    main.style.opacity = 0.5;
     header.style.opacity = 0.5;
 }
 
-function clousePopup() {
+function closePopup() {
     popup.classList.remove("popup_opened");
-    main.style.opacity = 1;
-    header.style.opacity = 1;
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
     title.textContent = nameInput.value;
     subtitle.textContent = jobInput.value;
-    main.style.opacity = 1;
-    header.style.opacity = 1;
-    clousePopup();
+    closePopup();
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
 buttonEdit.addEventListener("click", openPopup);
-popupClouse.addEventListener("click", clousePopup);
+popupClose.addEventListener("click", closePopup);
 
 
 
